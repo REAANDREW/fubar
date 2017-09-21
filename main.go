5c02eb4e2ffdc9669a9de1dbb719a074a3b4adcb
Andrew Rea <email@andrewrea.co.uk>
2017-09-21 21:02:44 +0000

    Adding notify for systemd

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/coreos/go-systemd/daemon"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -11,5 +13,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", handler)
+	daemon.SdNotify(false, "READY=1")
 	fmt.Println(http.ListenAndServe(":80", nil))
 }
